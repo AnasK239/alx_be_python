f6b7b85a4be4d95346190e9cb58797e03b42115b
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-13 20:36:02 +0300

    zzzzzzzzzzzzz

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+
+
+
+
+number_1 = 10
+
+number_2 = 5
+
+print(f"Addition of {number_1} and {number_2} is {number_1 + number_2}")
+print(f"Subtraction of {number_1} and {number_2} is {number_1 - number_2}")
+print(f"Multiplication of {number_1} and {number_2} is {number_1 * number_2}")
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+
+
+curr = int(input())
+
+print(f"In 2050, you will be {curr + 27} years old.")
