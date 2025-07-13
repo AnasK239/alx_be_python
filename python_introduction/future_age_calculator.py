73ee1f4c82680d9ea4c91c83ef2a93cbb359f2ee
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-13 20:47:04 +0300

    .

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,8 @@
 
 
-curr = int(input("How old are you now?"))
+curr = input("Enter your current age: ")
+while not curr.isdigit():
+    curr = input("Please enter a valid number for your current age: ")
+curr = int(curr)
 
 print(f"In 2050, you will be {curr + 27} years old.")
