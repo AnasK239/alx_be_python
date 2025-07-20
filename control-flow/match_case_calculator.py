11da5edd0e1bf006d9ec9b0801a5dea3c870e101
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-20 23:26:20 +0300

    ..

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,20 @@
+
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number):"))
+
+operation = input("Choose the operation (+, -, *, /):")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero is not allowed."
+
+print (f"The result is {result}")
