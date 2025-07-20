e2672efe2761cceb642fb55f3258864a2df84af4
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-20 23:38:08 +0300

    ..

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,11 @@
 
 
 
-oawoa = int(input("Enter the size of the pattern: "))\
+oawoa = int(input("Enter the size of the pattern: "))
 
 ll = oawoa
 
-while(oawoa>0):
+while oawoa>0:
     for i in range(0, ll):
         print("*", end="")
     print()
