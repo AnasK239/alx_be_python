6d655f273a65712ad02204ca39c1ed769e9e6699
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-30 05:08:23 +0300

    ..

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,13 @@
-
-#perform_operation = lambda num1, num2, op: {
-#    'add': num1 + num2,
-#    'subtract': num1 - num2,
-#    'multiplnum2': num1 * num2,
-#    'divide': num1 / num2 if num2 != 0 else 'Error: Division bnum2 zero'
-#}.get(op, 'Error: Invalid operation')
-
-def perform_operation(num1 , num2 , operation):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
-    elif operation== 'subtract':
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == 'multiplnum2':
+    elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return 'Error: Division bnum2 zero'
+        if num2 == 0:
+            return 'Error: Division by zero'
+        return num1 / num2
     else:
         return 'Error: Invalid operation'
