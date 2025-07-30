bc4057662912163cb29a5bd11ff17c9013be911b
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-07-30 05:17:59 +0300

    ..

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,10 +15,13 @@ def main():
         display_menu()
         choice = int(input("Enter your choice: "))
         if choice == 1:
-            shopping_list.append(input("Enter item to add: "))
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
             pass
         elif choice == 2:
-            shopping_list.remove(input("Enter item to remove: "))
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
             pass
         elif choice == 3:
             print(shopping_list)
